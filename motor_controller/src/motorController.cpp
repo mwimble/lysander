d9fc7bb6521aca230581d10298979ecea20193ea
wimble <mike@webself.com>
2018-09-27 19:52:37 -0700

    Formatted to ROS standard

--- motor_controller/src/motorController.cpp
+++ motor_controller/src/motorController.cpp
@@ -4,12 +4,13 @@
 #include "motorSkidSteerDrive.h"
 #include "motor_controller/motor_controllerConfig.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	ros::init(argc, argv, "motor_controller");
-	MotorSkidSteerDrive*  motorSkidSteerDrive = new MotorSkidSteerDrive();
+	MotorSkidSteerDrive *motorSkidSteerDrive = new MotorSkidSteerDrive();
 
 	dynamic_reconfigure::Server<motor_controller::motor_controllerConfig> server;
- 	dynamic_reconfigure::Server<motor_controller::motor_controllerConfig>::CallbackType f;
+	dynamic_reconfigure::Server<motor_controller::motor_controllerConfig>::CallbackType f;
 	f = boost::bind(&MotorSkidSteerDrive::configCallback, motorSkidSteerDrive, _1, _2);
 	server.setCallback(f);
 
@@ -20,13 +21,15 @@ int main(int argc, char **argv) {
 	int32_t old_m1_enc = 0;
 	int32_t old_m2_enc = 0;
 
-	while (ros::ok()) {
-		try {
+	while (ros::ok())
+	{
+		try
+		{
 			if ((old_batt != motorSkidSteerDrive->getLogicBatteryLevel()) ||
 				(old_m1_enc != motorSkidSteerDrive->getM1Encoder()) ||
-				(old_m2_enc != motorSkidSteerDrive->getM2Encoder())
-				) {
-				// ROS_INFO("[motor_controller] m1 enc: %d, m2 enc: %d, batt: %f", 
+				(old_m2_enc != motorSkidSteerDrive->getM2Encoder()))
+			{
+				// ROS_INFO("[motor_controller] m1 enc: %d, m2 enc: %d, batt: %f",
 				// 		 motorSkidSteerDrive->getM1Encoder(),
 				// 		 motorSkidSteerDrive->getM2Encoder(),
 				// 		 motorSkidSteerDrive->getLogicBatteryLevel());
@@ -37,11 +40,15 @@ int main(int argc, char **argv) {
 
 			ros::spinOnce();
 			r.sleep();
-		} catch(MotorSkidSteerDrive::TRoboClawException* e) {
+		}
+		catch (MotorSkidSteerDrive::TRoboClawException *e)
+		{
 			ROS_ERROR("[motor_controller] Exception: %s", e->what());
-		} catch(...) {
-		    ROS_ERROR("[motor_controller] Unhandled exception");
-        }
+		}
+		catch (...)
+		{
+			ROS_ERROR("[motor_controller] Unhandled exception");
+		}
 	}
 
 	return 0;
